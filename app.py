a00e1665b8865af6b04893bbaf129920a43982e1
Vegard755 <vss@hotmail.no>
2025-03-15 19:34:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
+def nocache(view):
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return response
+    return no_cache
+
+
 @app.route('/')
+@nocache
 def index():
     return render_template('index.html')
 
