37c66095b286f8eeff1400e20b9a0feb63bb3dc3
Vegard755 <vss@hotmail.no>
2025-03-15 23:15:25 +0100

    www

--- app.py
+++ app.py
@@ -4,20 +4,11 @@ import time
 
 app = Flask(__name__)
 
-def nocache(view):
-    def no_cache(*args, **kwargs):
-        response = make_response(view(*args, **kwargs))
-        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-        response.headers['Pragma'] = 'no-cache'
-        response.headers['Expires'] = '0'
-        return response
-    return no_cache
 
 
 
 
 @app.route('/')
-@nocache
 def index():
     timestamp = int(time.time())
     return render_template('index.html', timestamp=timestamp)
